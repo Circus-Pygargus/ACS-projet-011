7fa3f6edd9bb21d81e5c56802f7587ff87fe4e4d
Circus-Pygargus <richard.meuret@laposte.net>
2019-04-24 09:29:50 +0000

    formulaire fonctionnel

--- assets/js/myForm.js
+++ assets/js/myForm.js
@@ -9,44 +9,71 @@ var theMessage = document.querySelector("#theMessage");
 var fullNameErrorPlace = document.querySelector("#fullNameErrorPlace");
 var eMailErrorPlace = document.querySelector("#eMailErrorPlace");
 var theMessageErrorPlace = document.querySelector("#theMessageErrorPlace");
+var submitErrorPlace = document.querySelector("#submitErrorPlace");
 
 // ici c'est le submit
-var myForm = document.querySelector("myForm");
+var myForm = document.querySelector("#myForm");
 
-// les regEx
-var regExFullName = /^[a-zA-ZàâéèêëïôùûçÀÂÉÈÔÙÛÇ]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/gi;
-var regExEmail  = "";
-var regExtheMessage = "";
+// // les regEx
+// var regExFullName = new RegExp(/^[a-zA-ZàâéèêëîïôùûçÀÂÉÈÔÙÛÇ]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/gi);
+// var regExFullName = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u;
+
+/* caracters supported with next one :
+    abcdefghijklmnopqrstwxyz
+    ABCDEFGHIJKLMNOPQRSTUVWXYZ
+    áéíóúäëïöüÄ'
+    陳大文
+    łŁőŐűŰZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųū
+    ÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁ
+    ŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ.-
+    ñÑâê都道府県Федерации
+    আবাসযোগ্য জমির걸쳐 있는
+*/
+var regExFullName = /^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$/;
+var regExEmail  = /^(([^<>()[]\.,;:s@]+(.[^<>()[]\.,;:s@]+)*)|(.+))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
+var regExtheMessage = /.{3,}\s?/;
+
+// we'll need an error counter on inputs
+var errorInputCounter;
 
 
 /* ############################# eventListeners ###############################*/
 // full name input
 fullName.addEventListener("blur", function(){
-  checkInputContent (fullName, regExFullName,  fullNameErrorPlace, event);
+  checkInputContent(fullName, regExFullName,  fullNameErrorPlace, event, "fullName");
 });
 
 // email input
 eMail.addEventListener("blur", function(){
-  checkInputContent (eMail, regExEmail,  eMailErrorPlace, event);
+  checkInputContent(eMail, regExEmail,  eMailErrorPlace, event, "eMail");
 });
 
 // email input
 theMessage.addEventListener("blur", function(){
-  checkInputContent (theMessage, regExtheMessage, theMessageErrorPlace, event);
+  checkInputContent(theMessage, regExtheMessage, theMessageErrorPlace, event, "theMessage");
+});
+
+// form submit
+myForm.addEventListener("submit", function(_event){
+  checkEachInputAfterSubmit(event);
 });
 
 
+
 /* ############################### Functions #################################*/
+
 /* check input content function
     arguments :
       1 _inputToCheck : the input to check
       2 _regEx : the regular expression to compare with
       3 _errorPlace : the place to fill out with error message if needed
-      4 _event : l'évènement qui a déclenché l'action
+      4 _event : the event which has triggered the eventlistener
+      5 _whoIsChecked : this should be some text to know who we're talking about while sending error :
+            here : "fullName", "eMail" or "theMessage"
 */
-function checkInputContent (_inputToCheck, _regEx, _errorPlace, _event) {
-  // error counter
-  var errorInputCounter = 0;
+function checkInputContent (_inputToCheck, _regEx, _errorPlace, _event, _whoIsChecked) {
+  // reset error counter
+  errorInputCounter = 0;
   // first, prevent sending the form
   _event.preventDefault();
   // is the input empty ?
@@ -54,14 +81,24 @@ function checkInputContent (_inputToCheck, _regEx, _errorPlace, _event) {
     // increment the counter
     errorInputCounter++;
     // message under the input
-    _errorPlace.innerHTML = "Il faut renseigner ce champ !";
+    _errorPlace.innerHTML = "Please fill out this field !";
   }
-  // is the rexEx respected ?
-  else if (!_regEx.test(_inputToCheck.value)) {
+  // is the rexEx respected ?  
+  else if (!_inputToCheck.value.match(_regEx)) {
     // increment counter
     errorInputCounter++;
-    // message under the input
-    _errorPlace.innerHTML = "Tu sais pas écrire ton nom ?";
+    // need to know where error occured to give a custom error message
+    switch (_whoIsChecked) {
+      case "fullName" :
+        _errorPlace.innerHTML = "Could you please write your name correctly ?";
+        break;
+      case "eMail" :
+        _errorPlace.innerHTML = "Please give a valid email adress.";
+        break;
+      case "theMessage" :
+        _errorPlace.innerHTML = "your message has some invalid characters.";
+        break;
+    }
   }
 
   // is there any error ?
@@ -82,49 +119,32 @@ function checkInputContent (_inputToCheck, _regEx, _errorPlace, _event) {
 
 
 
-// eventListeners
-// fullName.addEventListener("blur", checkName);
-// eMail.addEventListener("blur", checkEmail);
-// theMessage.addEventListener("blur", checkMessage);
-// myForm.addEventListener("submit", checkAll);
+// a submit has been tried
+function checkEachInputAfterSubmit (_event) {
 
+  // first, prevent sending the form
+  _event.preventDefault();
 
-// // verifie le champ name
-// function checkName (event) {
-//   // on empêche l'envoi du formulaire
-//   event.preventdefault();
-//   // récup du contenu du champ
-//   var nameToCheck = fullName.value;
-//   // regEx
-//   // var regularExpression = 
-// }
+  // reset main error counter
+  var errorSubmitCounter = 0;
 
+  // check fields again and increment counter if error
+  checkInputContent(fullName, regExFullName,  fullNameErrorPlace, event, "fullName");
+  errorSubmitCounter += errorInputCounter;
+  checkInputContent(eMail, regExEmail,  eMailErrorPlace, event, "eMail");
+  errorSubmitCounter += errorInputCounter;
+  checkInputContent(theMessage, regExtheMessage, theMessageErrorPlace, event, "theMessage");
+  errorSubmitCounter += errorInputCounter;
 
-// // verifie le champ email
-// function checkEmail (event) {
-//   // on empêche l'envoi du formulaire
-//   event.preventdefault();
-//   // récup du contenu du champ
-//   var emailToCheck = eMail.value;
-// }
-
-
-// // vérifie le champ message
-// function checkMessage (event) {
-//   // on empêche l'envoi du formulaire
-//   event.preventdefault();
-//   // récup du contenu du champ
-//   var messageToCheck = theMessage.value;
-// }
-
-
-// // vérifie le tout
-// function checkAll (event) {
-//   // on commence par check tous les champs
-//   checkName();
-//   checkEmail();
-//   checkMessage();
-  
-//   // on empêche l'envoi du formulaire
-//   event.preventdefault();
-// }
+  // error detected
+  if (errorSubmitCounter != 0) {
+    // display a global error message
+    submitErrorPlace.style.color = "red";
+    submitErrorPlace.innerHTML = "Each field need to be valid to send the message.";
+  }
+  // no error, we can send the message to the server
+  else {
+    submitErrorPlace.style.color = "green";
+    submitErrorPlace.innerHTML = "Message would be sent if it was a real site ...";
+  }
+}
--- assets/js/myGalleryModal.js
+++ assets/js/myGalleryModal.js
@@ -2,3 +2,31 @@
        $('#galleryImage').attr("src",$(e.relatedTarget).data("large-src"));
       //  $('#galleryImage').h3.attr("value",$(e.relatedTarget).alt);
     });
+
+
+
+    
+    var hoverWatchedImgs = document.querySelectorAll(".myHoverWatcher");
+
+    console.log(hoverWatchedImgs);
+
+    // for (i=0; i<hoverWatchedImgs.length; i++) {
+    //   console.log(hoverWatchedImgs[i]);
+    //   this.onmouseover = function(){
+    //     console.log(this);
+    //     // console.log(event);
+    //     console.log(this.offsetParent);
+    //   };
+    // document.getElementById("demo").innerHTML = testDiv.offsetParent;
+    // }
+
+    
+    for (i=0; i<hoverWatchedImgs.length; i++) {
+      // console.log(hoverWatchedImgs[i]);
+      hoverWatchedImgs[i].addEventListener("mouseover", function(){
+        // console.log(this);
+        // console.log(event);
+        // console.log(this.offsetParent);
+        // console.log(this.offsetTop);
+      });
+    }
